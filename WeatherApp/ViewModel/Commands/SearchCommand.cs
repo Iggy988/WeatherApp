b6a984e7e41d82db8664a628510a67de3e7661c6
Iggy988 <igor.rodic1988@gmail.com>
2024-04-06 10:49:21 +0200

    Using a Command Parameter

--- WeatherApp/ViewModel/Commands/SearchCommand.cs
+++ WeatherApp/ViewModel/Commands/SearchCommand.cs
@@ -12,7 +12,12 @@ public class SearchCommand : ICommand
     public WeatherVM VM { get; set; }
 
     //when PropertyChanged event execute it will leteveryone using those properties that props are changed
-    public event EventHandler? CanExecuteChanged;
+    public event EventHandler? CanExecuteChanged
+    {
+        //add and remove value to event 
+        add { CommandManager.RequerySuggested += value; }
+        remove {  CommandManager.RequerySuggested -= value;}
+    }
 
     public SearchCommand(WeatherVM vm)
     {
@@ -21,6 +26,13 @@ public class SearchCommand : ICommand
 
     public bool CanExecute(object? parameter)
     {
+        string query = parameter as string;
+
+        //ako nema query - nije nista uneseno u polje - return false
+        if (string.IsNullOrWhiteSpace(query))
+        {
+            return false;
+        }
         return true;
     }
 
